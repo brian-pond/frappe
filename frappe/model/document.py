716b427017c6b0382168fe215bae1c48cba8fec9
Brian Pond <brian@datahenge.com>
2025-11-10 12:06:02 -0800

    Another datetime bug fix related to 'check_if_latest'

--- frappe/model/document.py
+++ frappe/model/document.py
@@ -926,6 +926,10 @@ class Document(BaseDocument):
 		if previous.modified and not isinstance(previous.modified, datetime_type):
 			raise TypeError(f"DocField \"previous.modified\" is a {type(previous.modified).__name__} but should a Type of datetime instead.")
 
+		if is_datetime_naive(previous.modified):
+			# Appears useful for situations like adding rows to tabSingles
+			previous.modified = localize_datetime(previous.modified, get_system_timezone())
+
 		# Unfortunately, at this point '_original_modified' could be a String or DateTime
 		if self._original_modified and not isinstance(self._original_modified, datetime_type):
 			from temporal_lib.tlib_types import any_to_datetime
